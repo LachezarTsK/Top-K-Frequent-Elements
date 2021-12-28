5dde2eac34db318def95226b31e1fb56f9be5751
Lachezar Kutsarov <lkutsarov@gmail.com>
2021-12-28 20:36:54 +0100

    Update Solution.cpp

--- Solution.cpp
+++ Solution.cpp
@@ -1,78 +1,74 @@
 
-# include<vector>
-# include<unordered_map>
+#include<vector>
+#include<unordered_map>
 
 using namespace std;
 
 class Solution {
-
 public:
-	unordered_map<int, int> frequency;
+    unordered_map<int, int> frequency;
 
-	vector<int> topKFrequent(vector<int>& nums, int k) {
+    vector<int> topKFrequent(vector<int>& nums, int k) {
 
-		fillMap_frequency(nums);
-		vector<int> uniqueInt;
+        fillMap_frequency(nums);
+        vector<int> uniqueInt;
 
-		for (auto& [key, value] : frequency) {
-			uniqueInt.push_back(key);
-		}
+        for (auto& [key, value] : frequency) {
+            uniqueInt.push_back(key);
+        }
 
-		if (uniqueInt.size() == k) {
-			return uniqueInt;
-		}
+        if (uniqueInt.size() == k) {
+            return uniqueInt;
+        }
 
-		return quickSelect(uniqueInt, k);
-	}
+        return quickSelect(uniqueInt, k);
+    }
 
-	vector<int> quickSelect(vector<int>& uniqueInt, int k) {
+    vector<int> quickSelect(vector<int>& uniqueInt, int k) {
 
-		int left = 0;
-		int right = uniqueInt.size() - 1;
-		int pivot = uniqueInt.size();
+        int left = 0;
+        int right = uniqueInt.size() - 1;
+        int pivot = uniqueInt.size();
 
-		while (pivot != k) {
-			pivot = partition(uniqueInt, left, right);
-			if (k > pivot) {
-				left = pivot;
-			}
-			else {
-				right = pivot - 1;
-			}
-		}
-		return vector<int>(uniqueInt.begin(), uniqueInt.begin() + k);
-	}
+        while (pivot != k) {
+            pivot = partition(uniqueInt, left, right);
+            if (k > pivot) {
+                left = pivot;
+            } else {
+                right = pivot - 1;
+            }
+        }
+        return vector<int>(uniqueInt.begin(), uniqueInt.begin() + k);
+    }
 
-	int partition(vector<int>& uniqueInt, int left, int right) {
+    int partition(vector<int>& uniqueInt, int left, int right) {
 
-		int pivotIndex = left + (right - left) / 2;
-		int pivotFrequency = frequency.at(uniqueInt[pivotIndex]);
+        int pivotIndex = left + (right - left) / 2;
+        int pivotFrequency = frequency.at(uniqueInt[pivotIndex]);
 
-		while (left < right) {
-			if (frequency.at(uniqueInt[left]) <= pivotFrequency) {
-				swap(uniqueInt[left], uniqueInt[right]);
-				right--;
-			}
-			else {
-				left++;
-			}
-		}
+        while (left < right) {
+            if (frequency.at(uniqueInt[left]) <= pivotFrequency) {
+                swap(uniqueInt[left], uniqueInt[right]);
+                right--;
+            } else {
+                left++;
+            }
+        }
 
-		if (frequency.at(uniqueInt[left]) > pivotFrequency) {
-			left++;
-		}
-		return left;
-	}
+        if (frequency.at(uniqueInt[left]) > pivotFrequency) {
+            left++;
+        }
+        return left;
+    }
 
-	void fillMap_frequency(vector<int>& nums) {
-		for (auto& n : nums) {
-			if (frequency.find(n) == frequency.end()) {
-				frequency[n] = 1;
-			}
-			else {
-				int update = frequency[n] + 1;
-				frequency[n] = update;
-			}
-		}
-	}
+    void fillMap_frequency(vector<int>& nums) {
+        for (auto& n : nums) {
+            if (frequency.find(n) == frequency.end()) {
+                frequency[n] = 1;
+            } else {
+                int update = frequency[n] + 1;
+                frequency[n] = update;
+            }
+        }
+    }
 };
